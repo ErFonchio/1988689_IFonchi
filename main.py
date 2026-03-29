4eca71b171d6bfe41a5990d7ad3b82a800a614a7
DISASTERSURGEON <corsale.1985903@studenti.uniroma1.it>
2026-03-29 21:47:42 +0200

    timestamp modify

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import psycopg2
 from nicegui import ui
 
+ui.label('Fonchi Dashboard')
+
 def get_connection():
     return psycopg2.connect(
         host="localhost",
@@ -15,7 +17,7 @@ def fetch_events():
     cur = conn.cursor()
 
     cur.execute("""
-        SELECT sensor_id, event_type, frequency, amplitude, timestamp
+        SELECT sensor_id, event_type, timestamp, frequency, amplitude 
         FROM events
         ORDER BY timestamp DESC
         LIMIT 50;
@@ -27,7 +29,31 @@ def fetch_events():
 
     return rows
 
-    
-ui.label('Fonchi Dashboard')
+def load_data():
+    data = fetch_events()
+
+    table.rows = [
+        {
+            "sensor": r[0],
+            "type": r[1],
+            "frequency": r[2],
+            "amplitude": r[3],
+            "timestamp": str(r[4]),
+        }
+        for r in data
+    ]
+
+table = ui.table(
+    columns=[
+        {'name': 'sensor', 'label': 'Sensor', 'field': 'sensor'},
+        {'name': 'type', 'label': 'Type', 'field': 'type'},
+        {'name': 'frequency', 'label': 'Frequency', 'field': 'frequency'},
+        {'name': 'amplitude', 'label': 'Amplitude', 'field': 'amplitude'},
+        {'name': 'timestamp', 'label': 'Timestamp', 'field': 'timestamp'},
+    ],
+    rows=[]
+)
+
+ui.button("Refresh data", on_click=load_data)
 
 ui.run()
